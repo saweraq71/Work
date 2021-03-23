abd61880eb1990fffd4efbabf58bcf26c041de37
George <sawera@bk.ru>
2021-03-23 16:35:57 +0300

    Добавьте файлы проекта.

---
+++ Work/Controllers/HomeController.cs
@@ -0,0 +1,124 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Work.Models;
+
+namespace Work.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ApplicationContext db;
+        public IWebHostEnvironment _appEnvironment;
+        public HomeController(ApplicationContext context, IWebHostEnvironment appEnvironment)
+        {
+            db = context;
+            _appEnvironment = appEnvironment;
+        }
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var FIO = from m in db.Users
+                      select m;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                FIO = FIO.Where(s => s.FIO.Contains(searchString));
+            }
+
+            return View(FIO);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Сreate(IFormFile uploadedFile, User user)
+        {
+            db.Users.Add(user);
+            if (uploadedFile != null)
+            {
+
+                string path = "/Files/" + uploadedFile.FileName;   // путь к папке Files
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))      // сохраняем файл в папку Files в каталоге wwwroot
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                user.PATH = path;
+                user.Imagename = uploadedFile.FileName;
+                db.SaveChanges();
+            }
+
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        /* 
+               public async Task<IActionResult> Dabataselist()
+               {
+                 return View(await db.Users.ToListAsync());
+               }
+       */
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(IFormFile uploadedFile,User user)
+        {
+        
+            if (uploadedFile != null)
+            {  
+                string path = "/Files/" + uploadedFile.FileName;   // путь к папке Files
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))      // сохраняем файл в папку Files в каталоге wwwroot
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                user.PATH = path;
+                user.Imagename = uploadedFile.FileName;
+            }   
+            db.Users.Update(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                {
+                    db.Users.Remove(user);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
+        }
+    }
+}
+
---
+++ Work/dbo.Users.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[Users] ON
+INSERT INTO [dbo].[Users] ([Id], [FIO], [POS], [TEL], [PATH], [Imagename]) VALUES (3, N'Филиппович Гоша Николаевич', N'Оператор', N'79645845316', NULL, NULL)
+INSERT INTO [dbo].[Users] ([Id], [FIO], [POS], [TEL], [PATH], [Imagename]) VALUES (4, N'Марценюк Дмитрий Сергеевич', N'Поставщик', N'+79645840312', N'/Files/depositphotos_12630302-stock-photo-3d-man-showing-thumbs-up.jpg', N'depositphotos_12630302-stock-photo-3d-man-showing-thumbs-up.jpg')
+INSERT INTO [dbo].[Users] ([Id], [FIO], [POS], [TEL], [PATH], [Imagename]) VALUES (5, N'Степанов Семен Сергеевич', N'Начальник отдела', N'+79543545723', N'/Files/bcc4151fc832965a4a648c665d16daf4.jpg', N'bcc4151fc832965a4a648c665d16daf4.jpg')
+SET IDENTITY_INSERT [dbo].[Users] OFF
